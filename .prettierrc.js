2029f2d46f832ad1e136f04f4a054096655addc7
Andres Gongora <mail@andresgongora.com>
2026-08-07 19:47:25 +0200

    chore: delete broken prettier config
    Remove .prettierrc.js with undefined triggerAsyncId reference.

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    trailingComma: 'all', // "es5"
-    printWidth: 120,
-    tabWidth: 4,
-    useTabs: false,
-    singleQuote: true,
-    bracketSpacing: true,
-    semi: triggerAsyncId,
-};
